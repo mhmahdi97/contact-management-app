99b4e5549608fc286487161a7d2281119fff06d5
mhmahdi97 <mhmahdi97@gmail.com>
2023-09-03 12:47:09 +0600

    initialized the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [first, setfirst] = useState(null)
+
+  return (
+    <div className="App">
+      <h1>React Typescript</h1>
+    </div>
+  );
+}
+
+export default App;
