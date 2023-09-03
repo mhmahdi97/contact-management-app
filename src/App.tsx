b7f43aaa590918223b53c8e0ba4b3ba00323f5c8
mhmahdi97 <mhmahdi97@gmail.com>
2023-09-03 17:40:34 +0600

    implemented dynamic title according to routes location on the title bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom";
+import Titlebar from "./Components/Titlebar";
 
 
 
@@ -6,21 +7,26 @@ function App() {
   
 
   return (   
-    <>  
+    <> 
+    <Titlebar></Titlebar>
       <div className="drawer lg:drawer-open">
 
-              <label htmlFor="my-drawer-2"  className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                  <span className="sr-only">Open sidebar</span>
-                  <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z" />
-                  </svg>
-              </label>
-          
+        {/* Button for open sidbar in smaller device */}
+          <label htmlFor="my-drawer-2"  className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+              <span className="sr-only">Open sidebar</span>
+              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z" />
+              </svg>
+          </label>
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+          {/* Pages from child routes render here  */}
           <div className="drawer-content flex flex-col items-center justify-center">
               <h1 className='text-4xl font-semibold mt-12'>Welcome to <span className='text-red-500'>The</span> Dahsboard!!!</h1>
               <Outlet></Outlet>
           </div> 
+
+          {/* sidebar drawer */}
           <div className="drawer-side">
               <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
               <ul className="menu p-4 w-80 h-full font-semibold bg-base-200 text-base-content">
@@ -33,7 +39,7 @@ function App() {
           
           </div>
       </div>
-  </>  
+    </>  
 
 
     
---
+++ src/Components/Titlebar.tsx
@@ -0,0 +1,20 @@
+
+import { useLocation } from "react-router-dom";
+
+
+const Titlebar = () => {
+    
+
+    const {pathname} = useLocation();
+    const title = pathname === '/contacts' ? 'Contacts Page' : 'Charts and Maps Page';
+    console.log(pathname);
+    
+
+    return (
+        <div className='h-16 bg-slate-400'>
+            <h1 className='text-white text-5xl text-center'>{title}</h1>
+        </div>
+    );
+};
+
+export default Titlebar;
